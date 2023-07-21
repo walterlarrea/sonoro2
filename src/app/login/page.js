3da6d2ddfd2a24a3ec6f5ac900053b78ea6d195c
Nicolás García <88388887+nicog33b@users.noreply.github.com>
2023-07-21 18:52:03 -0300

    21/7

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -8,6 +8,15 @@ const Login = () => {
 
   return (
     <>
+      <nav className="fixed top-0 right-0 bg-gray-800 p-4">
+      <ul className="space-y-4 text-white">
+     
+        <li>
+          <a href="#music" className="text-blue-300 hover:text-blue-100">Volver</a>
+        </li> 
+        {/* Add more navigation items as needed */}
+      </ul>
+    </nav>
       <div className="bg-gray-900 flex flex-col items-center justify-center h-screen">
         <h1 className="mb-8 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
           <span className="text-transparent bg-clip-text bg-gradient-to-r to-indigo-300 from-blue-600">Son</span>
