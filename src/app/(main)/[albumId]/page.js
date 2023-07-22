8e0e9f00c308be1d98e5e5ebfce8ac04cffe3d9e
Walter Larrea <walterlarrea@live.com>
2023-07-22 19:03:44 -0300

    fix: "use client" directive on a server component

--- src/app/(main)/[albumId]/page.js
+++ src/app/(main)/[albumId]/page.js
@@ -2,7 +2,6 @@
 import { useState, useEffect } from 'react';
 import { useParams } from "next/navigation";
 import { getAlbum } from '@/services/spotifyService';
-import RootLayout from '../../layout';
 
 const Album = () => {
   const router = useParams()
@@ -35,25 +34,23 @@ const Album = () => {
   }
 
   return (
-    <RootLayout>
-      <div className="relative w-content" >
+    <div className="relative w-content" >
 
-        <div className="absolute z-10 flex flex-col justify-end w-full bg-gradient-to-b from-gray-900 p-3 text-[1.25em]">
-          TEMONES en este album:
-          {album.tracks.items.map(t =>
-            <span>
-              {t.name}
-            </span>
-          )}
-        </div>
-
-        <img
-          src={album.images[0].url}
-          alt={`Cover of the ${album.album_type} ${album.name}`}
-          className='relative top-0 left-0 min-w-full w-full min-h-screen aspect-square'
-        />
+      <div className="absolute z-10 flex flex-col justify-end w-full bg-gradient-to-b from-gray-900 p-3 text-[1.25em]">
+        TEMONES en este album:
+        {album.tracks.items.map(t =>
+          <span>
+            {t.name}
+          </span>
+        )}
       </div>
-    </RootLayout>
+
+      <img
+        src={album.images[0].url}
+        alt={`Cover of the ${album.album_type} ${album.name}`}
+        className='relative top-0 left-0 min-w-full w-full min-h-screen aspect-square'
+      />
+    </div>
   )
 }
 
