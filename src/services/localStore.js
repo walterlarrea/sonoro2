53fcb05f7780f5fe01c070ccda3ac57fc2dcde96
Walter Larrea <walterlarrea@live.com>
2023-08-10 00:19:49 -0300

    fix: use of localStorage

--- src/services/localStore.js
+++ src/services/localStore.js
@@ -1,11 +1,17 @@
 export const setStore = (name, value) => {
-  localStorage.setItem(name, value);
+  if (typeof window !== "undefined") {
+    localStorage.setItem(name, value);
+  }
 };
 
 export const getStore = (name) => {
-  return localStorage.getItem(name);
+  if (typeof window !== "undefined") {
+    return localStorage.getItem(name);
+  }
 };
 
 export const removeStore = (name) => {
-  localStorage.removeItem(name);
+  if (typeof window !== "undefined") {
+    localStorage.removeItem(name);
+  }
 };
