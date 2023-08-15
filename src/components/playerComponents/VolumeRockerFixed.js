cbc18fc3b2b70d6df2381916b44f2f91de8f9772
Walter Larrea <walterlarrea@live.com>
2023-08-15 01:52:49 -0300

    style: parts of the bottom global player

--- src/components/playerComponents/VolumeRockerFixed.js
+++ src/components/playerComponents/VolumeRockerFixed.js
@@ -1,4 +1,5 @@
 import { usePlayerProvider } from "@/context/playerProvider";
+import { SpeakerWaveIcon } from "@heroicons/react/24/solid";
 
 const VolumeRockerFixed = () => {
   const { localVolume, setLocalVolume } = usePlayerProvider();
@@ -8,12 +9,16 @@ const VolumeRockerFixed = () => {
   }
 
   return (
-    <input
-      type='range'
-      min='0'
-      max='100'
-      defaultValue={localVolume * 100}
-      onChange={handleVolumeChange} />
+    <div className="flex items-center group gap-2 mx-4">
+      <SpeakerWaveIcon className="w-5 h-5 text-[#ffffff] group-hover:text-[#22C55E]" />
+      <input
+        className="group"
+        type='range'
+        min='0'
+        max='100'
+        defaultValue={localVolume * 100}
+        onChange={handleVolumeChange} />
+    </div>
   )
 }
 
