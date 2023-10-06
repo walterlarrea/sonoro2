f6f8ccf6edeb9a1efcb4f8d848733007079c60f6
Walter Larrea <85652281+walterlarrea@users.noreply.github.com>
2023-10-06 18:40:00 +0000

    fix: duplicated track id´s

--- src/components/TrackList.js
+++ src/components/TrackList.js
@@ -1,28 +1,20 @@
 'use client';
-import { useRouter } from "next/navigation";
 import TrackCard from "./TrackCard";
 
-const TrackList = ({ tracks, withAccess }) => {
-  const router = useRouter()
-
-  // const handleClick = (trackId) => () => {
-  //  if (withAccess) {
-  //    router.push(`/albums/${trackId}`)
-  //  }
-  // }
+const TrackList = ({ tracks }) => {
 
   return (
-    <div style={{
-      display: 'grid',
-      gridTemplateColumns: 'repeat(auto-fit, minMax(195px, 1fr))',
-      justifyItems: 'center',
-      gap: '1em',
-    }}
-      className="my-3"
-    >
+    <div
+      style={{
+        display: 'grid',
+        gridTemplateColumns: 'repeat(auto-fit, minMax(195px, 1fr))',
+        justifyItems: 'center',
+        gap: '1em',
+      }}
+      className="my-3">
 
       {tracks && tracks?.map(t =>
-        <div key={t.id} className="m-0 w-full min-w-[145px]">
+        <div key={t.played_at ?? t.id} className="m-0 w-full min-w-[145px]">
           <TrackCard
             track={t}
           // onClick={handleClick(t.id)}
